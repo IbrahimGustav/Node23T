25a7f584b6fa10c245fc288067517d7dd6e58bb9
IbrahimGustav <crthrsmrx@gmail.com>
2024-12-05 12:44:06 +0700

    a

--- userprofile/models/user-model.js
+++ userprofile/models/user-model.js
@@ -8,12 +8,5 @@ const getUserByID = async (id) =>{
     const [row] = await db.query('select *  from user where id=?',id)
     return row
 }
-const getUserByName = async (name) =>{
-    const [row] = await db.query('select * from user where name=?', name)
-    return row
-}
-const getUserByEmail = async (email) =>{
-    const [row] = await db.query('select * from user where email=?', email)
-}
 
 module.exports = {getAllUsers, getUserByID, getUserByName, getUserByEmail}
